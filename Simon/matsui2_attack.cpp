fbae853c018299b7597434114933e341017a9605
Erikct98 <e.c.takke@student.tue.nl>
2021-12-24 16:45:19 +0100

    update matsui2_attack

--- Simon/matsui2_attack.cpp
+++ Simon/matsui2_attack.cpp
@@ -5,19 +5,25 @@
 #include "simon.h"
 #include "encrypt.h"
 
-void attack(uint32_t nr_samples, uint16_t *subkeys, uint8_t rounds, uint32_t ipm, uint32_t imm, uint32_t opm, int32_t *counts)
+
+void attack(uint32_t nr_samples, uint16_t *subkeys, uint8_t rounds, uint32_t ipm, uint32_t imm, uint32_t opm, int32_t *counts, uint32_t nr_keys)
 {
-    uint16_t decrypt_key;
-    uint32_t ct, parity, dt;
-    for (uint32_t pt = 0; pt < nr_samples; pt++)
+    uint16_t decrypt_rounds = 2;
+    uint16_t decrypt_key[decrypt_rounds] = {0};
+    uint32_t pt, ct, parity, dt;
+    uint32_t a, b, c, d, e;
+    for (uint32_t i = 0; i < nr_samples; i++)
     {
+        pt = getRandomPt();
         ct = encrypt(pt, subkeys, rounds);
-        for (uint32_t j = 0; j < 4; j++)
+        for (uint32_t j = 0; j < nr_keys; j++)
         {
-            // decrypt_key = ((j << 2) & 0x0004) ^ ((j << 13) & 0x4000);
-            decrypt_key = j << 12;
-            dt = decrypt(ct, &decrypt_key, 1);
-            counts[j] += getParity((pt & ipm) ^ (dt & imm)) ^ 1;
+            a = j & 1, b = (j >> 1) & 1, c = (j >> 2) & 1, d = (j >> 3) & 1, e = (j >> 4) & 1;
+
+            decrypt_key[0] = (a << 2) ^ (b << 4) ^ (c << 11) ^ (d << 14);
+            decrypt_key[1] = (e << 12);
+            dt = decrypt(ct, decrypt_key, decrypt_rounds);
+            counts[j] += getParity((pt & ipm) ^ (dt & imm));
         }
     }
 }
@@ -28,12 +34,12 @@ int main()
 
     // Settings
     uint32_t correct_count = 0;
-    uint32_t rounds = 9;
+    uint32_t rounds = 7;
     uint32_t sample_size = 0x400000; // 2^22 = 4/(2^-10)^2 -> should give 99.9-ish% success rate.
-    uint32_t ipm = 0x44041000, imm = 0x40041000, opm = 0x10004404;
+    uint32_t ipm = 0x40040001, imm = 0x00014004, opm = 0x10004404;
     uint32_t trials = 0x40;
 
-    uint8_t nr_keys = 2;
+    uint32_t nr_keys = 32;
 
     uint64_t key;
     uint32_t guess;
@@ -46,14 +52,14 @@ int main()
 
         // Perform encryptions
         int32_t counts[nr_keys] = {0};
-        attack(sample_size, subkeys, rounds, ipm, imm, opm, counts);
+        attack(sample_size, subkeys, rounds, ipm, imm, opm, counts, nr_keys);
 
         // // Print counts to file
         // std::string fname = generateFileName("matsui2_attack", ipm, opm, sample_size, (uint32_t)0, rounds);
         // writeToFile(counts, 2, fname);
 
         // Print results
-        uint32_t keybit = (subkeys[rounds-1] >> 12) & 1;
+        uint32_t keybit = ((subkeys[rounds-2] >> 11) & (subkeys[rounds-2] >> 4)) & 1;
         std::cout << keybit << ' ';
         for (uint8_t j = 0; j < nr_keys; j++) {
             counts[j] = counts[j] - (sample_size >> 1);

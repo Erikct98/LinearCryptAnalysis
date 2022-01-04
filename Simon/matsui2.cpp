817955805fd4cc73bdd45a888ca9524b3261e7cb
Erikct98 <e.c.takke@student.tue.nl>
2022-01-04 18:39:07 +0100

    Refactor matsui2 attack

--- Simon/matsui2.cpp
+++ Simon/matsui2.cpp
@@ -6,24 +6,15 @@
 #include "simon.h"
 #include "encrypt.h"
 
-// Non-linear parity
-#define AND(x, i, y, j) ((x >> i) & (y >> j))
-
 // 5-round approximation SIMON32, used together with 7 encryption to achieve key.
 uint32_t computeNonLinearParity(uint32_t ct, uint16_t key)
 {
-    // const uint32_t X6R12 = ct >> 12 ^ ct >> 10 ^ ct >> 11 & ct >> 4;
-    // const uint32_t X6R01 = ct >> 12 ^ ct >> 10 ^ ct >> 11 & ct >> 4
-
-    return ((((ct >> 17) ^ (key >> 0) ^ (ct >> 15) ^ ((ct >> 0) & (ct >> 9))) // X7L[ 1] + k7[ 1] + X7R[15] + X7R[ 0] * X7R[ 9]
-             &
-             ((ct >> 26) ^ (key >> 2) ^ (ct >> 8) ^ ((ct >> 9) & (ct >> 2)))) // X7L[10] + k7[10] + X7R[ 8] + X7R[ 9] * X7R[ 2]
-            ^
-            (((ct >> 29) ^ (key >> 3) ^ (ct >> 11) ^ ((ct >> 12) & (ct >> 5))) // X7L[13] + k7[13] + X7R[11] + X7R[12] * X7R[ 5]
-             &
-             ((ct >> 22) ^ (key >> 1) ^ (ct >> 4) ^ ((ct >> 5) & (ct >> 14)))) // X7L[ 6] + k7[ 6] + X7R[ 4] + X7R[ 5] * X7R[14]
-            ) &
-           1;
+    const uint16_t X6R12_part = ct >> 11 & ct >> 4;                             // X7R[11] * X7R[ 4]
+    const uint32_t X6R01 = ct >> 17 ^ key >> 0 ^ ct >> 15 ^ ct >> 0 & ct >> 9;  // X7L[1] + k7[1] + X7R[15] + X7R[0] * X7R[9]
+    const uint32_t X6R10 = ct >> 26 ^ key >> 2 ^ ct >> 8 ^ ct >> 9 & ct >> 2;   // X7L[10] + k7[10] + X7R[8] + X7R[9] * X7R[2]
+    const uint32_t X6R13 = ct >> 29 ^ key >> 3 ^ ct >> 11 ^ ct >> 12 & ct >> 5; // X7L[13] + k7[13] + X7R[11] + X7R[12] * X7R[5]
+    const uint32_t X6R06 = ct >> 22 ^ key >> 1 ^ ct >> 4 ^ ct >> 5 & ct >> 14;  // X7L[6] + k7[6] + X7R[4] + X7R[5] * X7R[14]
+    return (X6R12_part ^ X6R01 & X6R10 ^ X6R13 & X6R06) & 1;
 }
 
 void attack(uint32_t rounds, uint32_t sample_size, uint64_t key)
@@ -44,7 +35,7 @@ void attack(uint32_t rounds, uint32_t sample_size, uint64_t key)
     {
         pt = random_uint32();
         ct = encrypt(pt, subkeys, rounds);
-        linear_parity = getParity(pt & ipm ^ ct & opm) ^ AND(ct, 11, ct, 4);
+        linear_parity = getParity(pt & ipm ^ ct & opm);
 
         for (uint32_t j = 0; j < nr_keys; j++)
         {
@@ -62,66 +53,29 @@ void attack(uint32_t rounds, uint32_t sample_size, uint64_t key)
     uint32_t masked = subkeys[6] & 0x2442;
     uint32_t correct_index = (masked >> 10 & 0x8) ^ (masked >> 8 & 0x4) ^ (masked >> 5 & 0x2) ^ (masked >> 1 & 0x1);
 
-    if ((counts[correct_index] != *maxval))
+    if (counts[correct_index] != *maxval)
     {
-        std::cout << "INCORRECT: ";
+        std::cout << "INCORRECT! GUESSED " << *maxval << ", WAS " << correct_index << std::endl;
         for (uint32_t i = 0; i < nr_keys; i++)
         {
             std::cout << i << "=" << counts[i] << ", ";
         }
-        std::cout << std::endl;
-        std::cout << "GUESSED: " << *maxval << " WAS: " << correct_index << std::endl
+        std::cout << std::endl
                   << std::endl;
     }
 }
 
 int main()
 {
+    // Settings
+    const uint32_t nr_iterations = 0x40;
     const uint32_t rounds = 7;
-    // const uint32_t sample_size = 0x400000; // 2^22 = 4/(2^-10)^2 -> should give 99.9-ish% success rate.
     const uint32_t sample_size = 0x4000;
 
-    for (uint16_t i = 0; i < 0x40; i++)
+    for (uint16_t i = 0; i < nr_iterations; i++)
     {
         attack(rounds, sample_size, random_uint64());
     }
 
     return 0;
-
-    // Settings
-    // uint32_t rounds = 7;
-    // uint32_t sample_size = 0x400000; // 2^22 = 4/(2^-10)^2 -> should give 99.9-ish% success rate.
-    // uint32_t ipm = 0x40040001, imm = 0x00014004, opm = 0x10004404;
-    // uint32_t trials = 0x40;
-
-    // uint32_t nr_keys = 32;
-
-    // uint64_t key;
-    // uint32_t guess;
-    // for (uint32_t i = 0; i < trials; i++)
-    // {
-    //     // Generate subkeys
-    //     key = random_uint64();
-    //     uint16_t subkeys[rounds] = {0};
-    //     generateSubKeys(key, subkeys, rounds);
-
-    //     // Perform encryptions
-    //     int32_t counts[nr_keys] = {0};
-    //     attack(sample_size, subkeys, rounds, ipm, imm, opm, counts, nr_keys);
-
-    //     // // Print counts to file
-    //     // std::string fname = generateFileName("matsui2_attack", ipm, opm, sample_size, (uint32_t)0, rounds);
-    //     // writeToFile(counts, 2, fname);
-
-    //     // Print results
-    //     uint32_t keybit = ((subkeys[rounds - 2] >> 11) & (subkeys[rounds - 2] >> 4)) & 1;
-    //     std::cout << keybit << ' ';
-    //     for (uint8_t j = 0; j < nr_keys; j++)
-    //     {
-    //         counts[j] = counts[j] - (sample_size >> 1);
-    //         std::cout << counts[j] << ' ';
-    //     }
-    //     std::cout << '\n';
-    // }
-    // return 0;
 }

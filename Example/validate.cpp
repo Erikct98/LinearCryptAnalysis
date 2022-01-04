9f0f65bcc0e4712c8fe3b5a00481185f7e8a02d1
Erikct98 <e.c.takke@student.tue.nl>
2022-01-04 12:54:22 +0100

    update testvector aesesque

--- Example/validate.cpp
+++ Example/validate.cpp
@@ -20,25 +20,40 @@ int main()
         0x00000000,
         0x00000000};
     uint32_t output[4] = {
-        0xfedd9e98,
-        0xf6542010,
-        0x0121c067,
-        0x8dab85eb};
+        0x2fbc2062,
+        0x88b906de,
+        0xbd167818,
+        0x929af016};
 
     // Encrypt
     encrypt(word, key, constants, 4);
 
+    // Check correctness
+    bool correct = true;
+    for (uint8_t i = 0; i < 4; i++)
+    {
+        correct &= word[0] == output[0];
+    }
+
     // Output result for verification
-    std::cout << "computed: "
-              << word[0] << ' '
-              << word[1] << ' '
-              << word[2] << ' '
-              << word[3] << std::endl;
-    std::cout << "expected: "
-              << output[0] << ' '
-              << output[1] << ' '
-              << output[2] << ' '
-              << output[3] << std::endl;
+    if (correct)
+    {
+        std::cout << "CORRECT" << std::endl;
+    }
+    else
+    {
+        std::cout << "INCORRECT" << std::endl;
+        std::cout << "computed: "
+                  << word[0] << ' '
+                  << word[1] << ' '
+                  << word[2] << ' '
+                  << word[3] << std::endl;
+        std::cout << "expected: "
+                  << output[0] << ' '
+                  << output[1] << ' '
+                  << output[2] << ' '
+                  << output[3] << std::endl;
+    }
 
     return 0;
 }

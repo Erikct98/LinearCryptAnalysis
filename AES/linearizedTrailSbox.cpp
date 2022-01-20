dd8d3fcddb4fab180b1f4d1d84b2db248a853522
Erikct98 <e.c.takke@student.tue.nl>
2022-01-20 10:37:57 +0100

    Add more approximations

--- AES/linearizedTrailSbox.cpp
+++ AES/linearizedTrailSbox.cpp
@@ -1,7 +1,5 @@
 #include "aes.h"
 
-#define OPM 0x74
-
 int I(uint8_t x)
 {
     return 1 - (x << 1);
@@ -20,11 +18,11 @@ uint8_t F(uint8_t x, uint8_t i)
 inline uint8_t getParity(uint8_t v)
 {
     v ^= v >> 4;
-    v &= 0xf;
+    v &= 0xF;
     return (0x6996 >> v) & 0x1;
 }
 
-uint8_t approx_7A_with_5(uint8_t word)
+bool approx_74_with_5(uint8_t word)
 {
     return (I(F(word, 1) ^ F(word, 2))\
          + I(F(word, 1) ^ F(word, 2) ^ F(word, 4) ^ F(word, 7))\
@@ -33,7 +31,21 @@ uint8_t approx_7A_with_5(uint8_t word)
          + I(F(word, 2) ^ F(word, 4)) ) >= 0;
 }
 
-uint8_t approx_7A_with_21(uint8_t word)
+bool approx_7A_with_5(uint8_t word)
+{
+    return (  I(F(word, 2) ^ F(word, 6))\
+            - I(F(word, 1) ^ F(word, 2) ^ F(word, 3) ^ F(word, 4))\
+            - I(F(word, 0) ^ F(word, 1))\
+            + I(F(word, 3) ^ F(word, 4) ^ F(word, 5) ^ F(word, 6))\
+            - I(F(word, 1) ^ F(word, 5) ^ F(word, 7))) >= 0;
+}
+
+uint8_t approx_7A_with_10(uint8_t pt)
+{
+    return (-16 * I(F(pt, 3) ^ F(pt, 5) ^ F(pt, 6)) + -16 * I(F(pt, 0) ^ F(pt, 1) ^ F(pt, 6) ^ F(pt, 7)) + -14 * I(F(pt, 0) ^ F(pt, 2) ^ F(pt, 3) ^ F(pt, 4) ^ F(pt, 5) ^ F(pt, 6) ^ F(pt, 7)) + -16 * I(F(pt, 3) ^ F(pt, 5) ^ F(pt, 7)) + -16 * I(F(pt, 0) ^ F(pt, 1) ^ F(pt, 3) ^ F(pt, 5) ^ F(pt, 7)) +  14 * I(F(pt, 3) ^ F(pt, 4) ^ F(pt, 5) ^ F(pt, 6)) + -14 * I(F(pt, 1) ^ F(pt, 5) ^ F(pt, 7)) +  14 * I(F(pt, 1) ^ F(pt, 2) ^ F(pt, 5)) + -16 * I(F(pt, 0) ^ F(pt, 1)) +  14 * I(F(pt, 0) ^ F(pt, 1) ^ F(pt, 2) ^ F(pt, 3) ^ F(pt, 7))) >= 0;
+}
+
+uint8_t approx_74_with_21(uint8_t word)
 {
     return (I(F(word, 0) ^ F(word, 2)) \
          + I(F(word, 0) ^ F(word, 1) ^ F(word, 2)) \
@@ -58,8 +70,63 @@ uint8_t approx_7A_with_21(uint8_t word)
          + I(F(word, 4) ^ F(word, 5) ^ F(word, 6) ^ F(word, 7))) >= 0;
 }
 
-void linearize_sbox_output_with_5_terms()
+uint8_t approx_7A_with_50(uint8_t pt)
 {
+    return ( 12 * I(F(pt, 1) ^ F(pt, 2) ^ F(pt, 4))
+          + -12 * I(F(pt, 0) ^ F(pt, 2) ^ F(pt, 3) ^ F(pt, 4) ^ F(pt, 5) ^ F(pt, 7))
+          + -16 * I(F(pt, 3) ^ F(pt, 5) ^ F(pt, 7))
+          + -12 * I(F(pt, 1) ^ F(pt, 6) ^ F(pt, 7))
+          +  12 * I(F(pt, 0) ^ F(pt, 1) ^ F(pt, 2) ^ F(pt, 4) ^ F(pt, 6) ^ F(pt, 7))
+          + -12 * I(F(pt, 1) ^ F(pt, 2) ^ F(pt, 3))
+          +  14 * I(F(pt, 1) ^ F(pt, 2))
+          +  12 * I(F(pt, 1) ^ F(pt, 4) ^ F(pt, 5) ^ F(pt, 6))
+          +  12 * I(F(pt, 0))
+          +  14 * I(F(pt, 0) ^ F(pt, 3) ^ F(pt, 4) ^ F(pt, 5) ^ F(pt, 7))
+          + -12 * I(F(pt, 1) ^ F(pt, 2) ^ F(pt, 3) ^ F(pt, 5))
+          +  12 * I(F(pt, 2) ^ F(pt, 6))
+          + -12 * I(F(pt, 1) ^ F(pt, 2) ^ F(pt, 3) ^ F(pt, 4) ^ F(pt, 6))
+          + -16 * I(F(pt, 0) ^ F(pt, 1) ^ F(pt, 6) ^ F(pt, 7))
+          +  14 * I(F(pt, 0) ^ F(pt, 1) ^ F(pt, 2) ^ F(pt, 3) ^ F(pt, 7))
+          + -14 * I(F(pt, 1) ^ F(pt, 2) ^ F(pt, 3) ^ F(pt, 4))
+          + -12 * I(F(pt, 0) ^ F(pt, 3) ^ F(pt, 4) ^ F(pt, 6) ^ F(pt, 7))
+          + -12 * I(F(pt, 1) ^ F(pt, 3) ^ F(pt, 7))
+          +  14 * I(F(pt, 3))
+          +  12 * I(F(pt, 3) ^ F(pt, 4) ^ F(pt, 6) ^ F(pt, 7))
+          +  14 * I(F(pt, 0) ^ F(pt, 1) ^ F(pt, 2) ^ F(pt, 3) ^ F(pt, 4) ^ F(pt, 5))
+          +  14 * I(F(pt, 3) ^ F(pt, 4) ^ F(pt, 5) ^ F(pt, 6))
+          +  12 * I(F(pt, 2) ^ F(pt, 3) ^ F(pt, 4) ^ F(pt, 5) ^ F(pt, 7))
+          +  12 * I(F(pt, 3) ^ F(pt, 7))
+          + -12 * I(F(pt, 0) ^ F(pt, 4) ^ F(pt, 5) ^ F(pt, 6))
+          + -16 * I(F(pt, 3) ^ F(pt, 5) ^ F(pt, 6))
+          + -12 * I(F(pt, 1) ^ F(pt, 2) ^ F(pt, 7))
+          + -16 * I(F(pt, 0) ^ F(pt, 1) ^ F(pt, 3) ^ F(pt, 5) ^ F(pt, 7))
+          + -12 * I(F(pt, 1) ^ F(pt, 2) ^ F(pt, 3) ^ F(pt, 4) ^ F(pt, 5) ^ F(pt, 7))
+          +  12 * I(F(pt, 0) ^ F(pt, 1) ^ F(pt, 5) ^ F(pt, 6) ^ F(pt, 7))
+          + -14 * I(F(pt, 0) ^ F(pt, 1) ^ F(pt, 2))
+          + -14 * I(F(pt, 0) ^ F(pt, 1) ^ F(pt, 7))
+          + -12 * I(F(pt, 0) ^ F(pt, 2) ^ F(pt, 3) ^ F(pt, 6) ^ F(pt, 7))
+          +  12 * I(F(pt, 3) ^ F(pt, 4))
+          + -14 * I(F(pt, 1) ^ F(pt, 5) ^ F(pt, 7))
+          + -12 * I(F(pt, 0) ^ F(pt, 4) ^ F(pt, 7))
+          +  12 * I(F(pt, 0) ^ F(pt, 3) ^ F(pt, 5) ^ F(pt, 6))
+          +  12 * I(F(pt, 0) ^ F(pt, 2) ^ F(pt, 4))
+          + -14 * I(F(pt, 0) ^ F(pt, 1) ^ F(pt, 3) ^ F(pt, 4) ^ F(pt, 7))
+          +  12 * I(F(pt, 2) ^ F(pt, 7))
+          + -12 * I(F(pt, 1) ^ F(pt, 3) ^ F(pt, 5) ^ F(pt, 7))
+          + -12 * I(F(pt, 2) ^ F(pt, 3) ^ F(pt, 4) ^ F(pt, 6))
+          + -16 * I(F(pt, 0) ^ F(pt, 1))
+          + -12 * I(F(pt, 1) ^ F(pt, 2) ^ F(pt, 4) ^ F(pt, 5))
+          + -12 * I(F(pt, 2) ^ F(pt, 3) ^ F(pt, 4) ^ F(pt, 7))
+          + -12 * I(F(pt, 2) ^ F(pt, 4) ^ F(pt, 5))
+          +  12 * I(F(pt, 0) ^ F(pt, 3) ^ F(pt, 4) ^ F(pt, 5) ^ F(pt, 6) ^ F(pt, 7))
+          + -12 * I(F(pt, 0) ^ F(pt, 3) ^ F(pt, 7))
+          +  14 * I(F(pt, 1) ^ F(pt, 4) ^ F(pt, 5) ^ F(pt, 6) ^ F(pt, 7))
+          + -14 * I(F(pt, 1) ^ F(pt, 3) ^ F(pt, 5))) >= 0;
+}
+
+void linearize_sbox_output_7A_with_5_terms()
+{
+    uint8_t OPM = 0x7A;
     uint8_t pt, ct;
     uint16_t count = 0;
     int linearized;
@@ -68,29 +135,81 @@ void linearize_sbox_output_with_5_terms()
         ct = SubByte(pt);
         count += (approx_7A_with_5(pt) ^ getParity(ct & OPM)) & 1;
     }
-    std::cout << "--- 5 TERMS ---" << std::endl;
+    std::cout << "--- 7A - 5 TERMS ---" << std::endl;
     std::cout << "count: " << count << " / " << 256 << std::endl;
     std::cout << "corr: " << (2 * (count / 256.0) - 1) << std::endl;
 }
 
-void linearize_sbox_output_with_21_terms()
+void linearize_sbox_output_74_with_5_terms()
 {
+    uint8_t OPM = 0x74;
+    uint8_t pt, ct;
+    uint16_t count = 0;
+    int linearized;
+    for (uint16_t pt = 0; pt < 0x100; pt++)
+    {
+        ct = SubByte(pt);
+        count += (approx_74_with_5(pt) ^ getParity(ct & OPM)) & 1;
+    }
+    std::cout << "--- 74 - 5 TERMS ---" << std::endl;
+    std::cout << "count: " << count << " / " << 256 << std::endl;
+    std::cout << "corr: " << (2 * (count / 256.0) - 1) << std::endl;
+}
+
+void linearize_sbox_output_7A_with_10_terms()
+{
+    uint8_t OPM = 0x7A;
+    uint8_t pt, ct;
+    uint16_t count = 0;
+    int linearized;
+    for (uint16_t pt = 0; pt < 0x100; pt++)
+    {
+        ct = SubByte(pt);
+        count += (approx_7A_with_10(pt) ^ getParity(ct & OPM)) & 1;
+    }
+    std::cout << "--- 7A - 10 TERMS ---" << std::endl;
+    std::cout << "count: " << count << " / " << 256 << std::endl;
+    std::cout << "corr: " << (2 * (count / 256.0) - 1) << std::endl;
+}
+
+void linearize_sbox_output_74_with_21_terms()
+{
+    uint8_t OPM = 0x74;
     uint8_t ct;
     uint16_t count = 0;
     int linearized;
     for (uint16_t pt = 0; pt < 0x100; pt++)
     {
         ct = SubByte(pt);
-        count += (approx_7A_with_21(pt) ^ getParity(ct & OPM)) & 1;
+        count += (approx_74_with_21(pt) ^ getParity(ct & OPM)) & 1;
     }
-    std::cout << "--- 21 TERMS ---" << std::endl;
+    std::cout << "--- 74 - 21 TERMS ---" << std::endl;
+    std::cout << "count: " << count << " / " << 256 << std::endl;
+    std::cout << "corr: " << (2 * (count / 256.0) - 1) << std::endl;
+}
+
+void linearize_sbox_output_7A_with_50_terms()
+{
+    uint8_t OPM = 0x7A;
+    uint8_t ct;
+    uint16_t count = 0;
+    int linearized;
+    for (uint16_t pt = 0; pt < 0x100; pt++)
+    {
+        ct = SubByte(pt);
+        count += (approx_7A_with_50(pt) ^ getParity(ct & OPM)) & 1;
+    }
+    std::cout << "--- 7A - 50 TERMS ---" << std::endl;
     std::cout << "count: " << count << " / " << 256 << std::endl;
     std::cout << "corr: " << (2 * (count / 256.0) - 1) << std::endl;
 }
 
 int main()
 {
-    linearize_sbox_output_with_5_terms();
-    linearize_sbox_output_with_21_terms();
+    linearize_sbox_output_7A_with_5_terms();
+    linearize_sbox_output_7A_with_10_terms();
+    linearize_sbox_output_7A_with_50_terms();
+    linearize_sbox_output_74_with_5_terms();
+    linearize_sbox_output_74_with_21_terms();
     return 0;
 }

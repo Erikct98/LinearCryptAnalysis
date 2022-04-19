7512c2798727c02b899cf68b0df1130ac9e2ea6e
Erikct98 <e.c.takke@student.tue.nl>
2022-04-19 14:05:25 +0200

    Analyse montecarlo results

--- PyAES/analyseMonteCarlo.py
+++ PyAES/analyseMonteCarlo.py
@@ -5,12 +5,12 @@ Analyses the results from the MonteCarlo tests.
 import json
 import math
 from correlation import keyed_func_corr
-from toolbox import GF2_8, I, split32, P8
+from toolbox import GF2_8, I, join32, split32, P8
 from mixcolumn_trail_search import find_input_mask
-from typing import Dict
-from mmm import MM5_input_masks, core_input_masks_v2, majority_func_for_masks
+from typing import Dict, List
+from mmm import MM5_input_masks, core_input_masks_v2
 
-FNAME = "../AES/montecarloresults3.json"
+FNAME = "../AES/montecarloresults6.json"
 
 IPM = find_input_mask(0x2D000000)
 
@@ -29,10 +29,10 @@ def keycategory(opm, key):
 
     return " ".join(categories)
 
-def expected_correlation(guess, actual):
+def expected_correlation(guess: int, actual: List[int]):
     opms = split32(IPM)
     guess_keys = split32(guess)
-    actual_keys = split32(actual)
+    actual_keys = [split32(k)[i] for i, k in enumerate(actual)]
 
 
     corrs = []
@@ -46,7 +46,7 @@ def expected_correlation(guess, actual):
 
         corr128 = keyed_func_corr(ipf, opf, ipk=ka, opk=0)
         corrs.append(corr128 / 128)
-    print(corrs)
+    # print(corrs)
 
     return math.prod(corrs) / 8
 
@@ -63,23 +63,32 @@ def analyse():
 
     trails = data['trails']
     for tid, trail in enumerate(trails):
-        trail_key = int(trail['key'][2:], 16)
+        trail_key = [int(x[2:], 16) for x in trail['key']]
+        tk = join32(*[split32(k)[i] for i, k in enumerate(trail_key)])
 
-        print(f"TRAIL {tid}, with trail_key = 0x{trail_key:08X}")
+        print(f"TRAIL {tid:02}: ", end="")
+        # print(f"TRAIL {tid}, with trail_key = {[f'0x{k:08X}' for k in trail_key]}")
 
         counts : Dict[str, int] = trail['counts']
         counts = {
             int(k[2:], 16): v for k, v in counts.items()
         }
 
+        corrs = {}
         for kg, cnt in counts.items():
             corr = 2 * cnt / sample_size - 1
             corrlog = math.log2(abs(corr)) if corr != 0 else - math.inf
-            expectedcorr = expected_correlation(kg, trail_key)
-            expectedcorrlog = math.log2(abs(expectedcorr)) if expectedcorr != 0 else - math.inf
-            # expectedcorr = math.log2(abs())
-            print(f"  kg=0x{kg:08X}, cat={keycategory(0x01, kg)}, corr={corrlog:.2f}, exp={expectedcorrlog:.2f}")
+            corrs[kg] = corrlog
+            # expectedcorr = expected_correlation(kg, trail_key)
+            # expectedcorrlog = math.log2(abs(expectedcorr)) if expectedcorr != 0 else - math.inf
+            # print(f"  kg=0x{kg:08X}, cat={keycategory(0x01, kg)}, corr={corrlog:.2f}, exp={expectedcorrlog:.2f}")
 
+
+        items = list(corrs.items())
+        actual = items[0][1]
+        mapping = sorted([(y,x) for x,y in items[1:]])
+
+        print(f"actual key: {hex(tk)}, key guess: {hex(items[0][0])}, corr: {actual:.2f}, best random key: {hex(mapping[-1][1])}, corr: {mapping[-1][0]:.2f}")
 
         # print(tid, trail, counts)
         # print(tid, trail, f"0x{trail_key:08X}")

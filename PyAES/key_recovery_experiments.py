5986ff568ccc1baa315d563bbe7a9afe4e5250d0
Erikct98 <e.c.takke@student.tue.nl>
2022-05-04 09:22:31 +0200

    Key Recovery improvements

--- PyAES/key_recovery_experiments.py
+++ PyAES/key_recovery_experiments.py
@@ -2,57 +2,19 @@
 This library provides tools that are useful in trailing through AES in case
 of non-zero keys and extracting keybits in the process.
 """
+import math
 import random
+from typing import Dict, List, Tuple
 from core.mmm import core_input_masks
-from core.correlation import corr, keyed_func_corr
-from core.toolbox import I, J, P8, GF2_8, bit_count, signum, subbyte
-from core.LAT import LAT
-from linearization import keyed_linearization_for_ipm
+from core.correlation import corr
+from core.toolbox import P8, GF2_8, signum, subbyte
 from collections import Counter
 
+Vector = Tuple[int, int, int, int]
+Key = Tuple[int, int, int]
+Inequalities = Tuple[int, int, int, int]
 
-def compute_correlations_wo_key(key_guess):
-    results = [list() for _ in GF2_8]
-    for ipm in GF2_8:
-        for key in GF2_8:
-            def ipf(pt): return P8(pt & ipm)
-            opf = keyed_linearization_for_ipm(ipm, key_guess)
-            results[ipm].append(keyed_func_corr(ipf, opf, 0x0, key))
-        print(results[ipm], Counter(results[ipm]))
-    return results
-
-def KAT(guess: int, key:int, opm:int) -> int:
-    """
-    Key Approximation Table
-    :param guess: guessed key
-    :param key: actual encryption key
-    :param opm: output mask.
-    :returns: correlation between guess and key for opm.
-    """
-    GAT = [LAT[ipm][opm] * I(P8(ipm & guess)) for ipm in GF2_8]
-    KAT = [LAT[ipm][opm] * I(P8(ipm & key)) for ipm in GF2_8]
-    corr128 = 128
-    for pt in GF2_8:
-        exp_par = J(sum(GAT[ipm] * I(P8(ipm & pt)) for ipm in GF2_8) >> 7)
-        act_par = J(sum(KAT[ipm] * I(P8(ipm & pt)) for ipm in GF2_8) >> 7)
-        corr128 -= exp_par ^ act_par
-    return corr128
-
-
-def compute_key_byte(a):
-    total = sum(map(lambda x: abs(x[1]), a))
-    counts = [sum(map(lambda x : int((f'{x[0]:08b}')[i]) * x[1], a)) - total/2 for i in range(8)]
-    print(counts)
-    return int(''.join([str(int(counts[i] >= 0)) for i in range(8)]), 2)
-
-def analyse_KAT():
-    results = [KAT(guess, 0xfa, 0x01) for guess in GF2_8]
-    cors = set(x for x in results)
-    for cor in cors:
-        print(f'{cor:4}: {sorted([(f"{i:08b}", bit_count(i)) for i in GF2_8 if results[i] == cor], key=lambda x: x[1])}')
-
-
-LUT = {
+INEQtoKEY: Dict[Inequalities, Key] = {
     (1, 1, 1, -1): (0, 0, 0),
     (1, 1, -1, 1): (0, 0, 1),
     (1, -1, 1, 1): (0, 1, 0),
@@ -62,48 +24,102 @@ LUT = {
     (-1, -1, 1, -1): (1, 1, 0),
     (-1, -1, -1, 1): (1, 1, 1)
 }
+KEYtoINEQ: Dict[Key, Inequalities] = {v: k for k, v in INEQtoKEY.items()}
 
-def solve_system(vectors):
-    # print("======")
+
+def _manhattan(a: Vector, b: Vector) -> float:
+    """
+    Compute the manhattan distance between vectors `a` and `b`.
+    Assumes both vectors have the same size.
+    """
+    assert len(a) == len(b)
+    return sum(abs(x - y) for x, y in zip(a, b))
+
+
+def manhattan(vectors: List[Vector]) -> Dict[Key, float]:
+    """
+    Likelihood estimator for key, based on Manhattan distance metric.
+    """
+
+    # Compute coordinate
+    vector = []
     c = Counter(vectors)
-    test_vectors = [(0, 0), (0, 1), (1, 0), (1, 1)]
+    for v in [(0, 0, 0), (0, 0, 1), (0, 1, 0), (0, 1, 1)]:
+        x, y = c.get((*v, 0), 0), c.get((*v, 1), 0)
+        try:
+            vector.append((x - y) / (x + y))
+        except ZeroDivisionError:
+            vector.append(0)
 
-    # Extract geq guesses and magnitudes
-    guesses = []
-    for vector in test_vectors:
-        x, y = c.get((0, *vector, 0), 0), c.get((0, *vector, 1), 0)
-        guesses.append(x - y)
-    # print("guesses:", guesses)
+    # Compute manhattan distance metric.
+    manhattan_metric = {key: _manhattan(ineq, vector)
+                        for ineq, key in INEQtoKEY.items()}
 
-    # # CAN WE INVOLVE THE ENTRY WITH THE GREATEST CONFIDENCE?
-    # # Extreme entry
-    # ee = max([(i,v) for i, v in enumerate(guesses)], key=lambda x: abs(x[1]))
-    # print("extreme entry:", ee)
-    # signs = (0, *test_vectors[ee[0]])
-    # if ee[1] < 0:
-    #     signs = tuple(sign ^ 1 for sign in signs)
-    # return signs
+    return manhattan_metric
 
-    # See if this does the trick
-    signs = [signum(x) for x in guesses]
-    # print("signs before", signs)
 
-    if abs(sum(signs)) != 2:
-        # See which guess has to be flipped.
-        m = sorted(guesses, key=lambda x: abs(x))[0]
-        indexes = [(i, x) for i, x in enumerate(guesses) if x == m]
-        if len(indexes) == 1:
-            idx = guesses.index(m)
-        else:
-            # Better tiebreaker.
-            v = max(indexes, key=lambda x: abs(guesses[x[0]]))[0]
-            idx = indexes.index((v, m))
+def choose(n, k):
+    "Returns n choose k"
+    return int(math.perm(n, k) / math.factorial(k))
 
-        signs[idx] = signs[idx] * -1
 
-    # print("signs after ", signs)
+def CDF_binomial(p, n, k) -> float:
+    """
+    Compute CDF of binomial distribution.
+    :param p: probability of success.
+    :param n: number of samples.
+    :param k: number of successes.
+    """
+    return sum(
+        choose(n, i) * (p ** i) * (1 - p) ** (n - i)
+        for i in range(k + 1)
+    )
 
-    return LUT[tuple(signs)]
+
+def gap(vectors: List[Vector]) -> Dict[Inequalities, float]:
+    """
+    Likelihood estimator for inequalities, based on gap-size.
+    """
+    gap_metric = {}
+
+    # Compute gap probabilities
+    c = Counter(vectors)
+    for v in [(0, 0, 0), (0, 0, 1), (0, 1, 0), (0, 1, 1)]:
+        k, n_k = c.get((*v, 0), 0), c.get((*v, 1), 0)
+        n = k + n_k
+
+        # Case: >
+        prob_h0 = 1 - CDF_binomial(38/64, n, k - 1)
+        prob_h1 = CDF_binomial(42/64, n, k)
+        gap_metric[v] = prob_h1 / (prob_h0 + prob_h1)  # Normalized
+
+        # Case: <
+        nv = tuple(i ^ 1 for i in v)
+        prob_h0 = 1 - CDF_binomial(38/64, n, n_k - 1)
+        prob_h1 = CDF_binomial(42/64, n, n_k)
+        gap_metric[nv] = prob_h1 / (prob_h0 + prob_h1)  # Normalized
+
+    # Normalize
+    prob_sum = sum(gap_metric.values())
+    gap_metric = {k: v / prob_sum for k, v in gap_metric.items()}
+
+    return gap_metric
+
+
+def guess_3bit_key(vectors: List[Tuple[int, int, int, int]]) -> Tuple[int, int, int]:
+    """
+    Guess three key bits based on the observed `vectors`.
+    """
+    metric = manhattan(vectors)
+    print("==============")
+    for k,v in metric.items():
+        print(f"{k}: {v:.2f}")
+    metric = gap(vectors)
+    print("--------------")
+    for k,v in metric.items():
+        print(f"{k}: {v:.2f}")
+    m = max(metric.items(), key=lambda x: x[1])
+    return m[0]
 
 
 def linear_system_solving_esque(ss: int, exps):
@@ -119,12 +135,10 @@ def linear_system_solving_esque(ss: int, exps):
     for _ in range(exps):
         # Select key
         key = random.choice(GF2_8)
-        # key = 1
         key_parities = tuple(P8(key & ipm) for ipm in ipms)
 
         # Create data
         pts = [random.choice(GF2_8) for _ in range(ss)]
-        # pts = GF2_8
         cts = [subbyte(p ^ key) for p in pts]
 
         # Compute equations
@@ -143,18 +157,22 @@ def linear_system_solving_esque(ss: int, exps):
             res.append((*ipv, op))
 
         # Indicate expected key parities
-        exp_key_parities = solve_system(res)
+        exp_key_parities = guess_3bit_key(res)
+        # print("actual:", invLUT[key_parities])
         # print("guessed parities:", exp_key_parities)
         # print("parities:", key_parities)
 
         # Verify expectations
         distance = sum(i ^ j for i, j in zip(key_parities, exp_key_parities))
+        if distance >= 1:
+            print(f"{exp_key_parities} should be {key_parities}")
         off_by[distance] += 1
 
-    print("\nRESULTS:")
+    print(f"SETTINGS:\n  sample size = {ss}\n  #experiments = {exps}")
+    print("RESULTS:")
     for i, x in enumerate(off_by):
-        print(f"off by {i}: {x} / {exps} = {x / exps * 100:.2f}%")
+        print(f"  off by {i}: {x} / {exps} = {x / exps * 100:.2f}%")
 
 
 if __name__ == "__main__":
-    linear_system_solving_esque(16, 65536)
+    linear_system_solving_esque(16, 128)
